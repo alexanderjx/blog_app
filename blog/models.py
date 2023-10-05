395bcb13d413163541a34e46de3a339b4256d071
Jux <>
2023-10-05 08:52:04 -0400

    styling

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
-        "auth.User",
+        'auth.User',
         on_delete=models.CASCADE,
     )
     body = models.TextField()
@@ -16,4 +16,4 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs={"pk": self.pk})
+        return reverse('post_detail', kwargs={'pk': self.pk})
